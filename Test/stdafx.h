21e4b10de919c93f11d65e16be1dac1c5858eccc
insoLLLent <vitaly@lifanoff.ru>
2019-08-31 08:21:05 +0300

    added libs to precompiled headers

--- Test/stdafx.h
+++ Test/stdafx.h
@@ -3,6 +3,16 @@
 #include <limits>
 #include <ostream>
 #include <vector>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <numeric>
+#include <utility>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
 
 #pragma warning(push)
 #pragma warning(disable:26495)
